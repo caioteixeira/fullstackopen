8858011bd0888b0114c96efd1466c95b0dcae0ff
Caio Teixeira <caiovmteixeira@outlook.com>
2021-05-04 20:11:28 -0300

    4.4: helper functions and unit tests, step2

--- part4/blog-list-backend/utils/list_helper.js
+++ part4/blog-list-backend/utils/list_helper.js
@@ -1,8 +1,19 @@
-const dummy = (blogs) => {
+const dummy = () => {
   // ...
   return 1
 }
 
+const totalLikes = (blogs) => {
+  const reducer = (accumulator, currentValue) => accumulator + currentValue
+  const likes = blogs.map(blog => blog.likes)
+
+  if(likes.length == 0) {
+    return 0
+  }
+
+  return likes.reduce(reducer)
+}
+
 module.exports = { 
-  dummy
+  dummy, totalLikes
 }
