57144ddc7166969dd83da4a992cec5d22ad1e14b
Caio Teixeira <caiovmteixeira@outlook.com>
2021-05-11 19:14:39 -0300

    4.12: Validate required fields

--- part4/blog-list-backend/tests/blog_api.test.js
+++ part4/blog-list-backend/tests/blog_api.test.js
@@ -15,81 +15,87 @@ beforeEach(async () => {
   await Promise.all(promiseArray)
 })
 
-test('blogs are return as json', async () => {
-  await api
-    .get('/api/blogs')
-    .expect(200)
-    .expect('Content-Type', /application\/json/)
+describe('when there is initially some blogs saved' , () => {
+  test('blogs are return as json', async () => {
+    await api
+      .get('/api/blogs')
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+  })
+  
+  test('all blogs are returned', async () => {
+    const response = await api.get('/api/blogs')
+  
+    expect(response.body).toHaveLength(helper.initialBlogs.length)
+  })
+  
+  test('a specific title is within the returned blogs', async () => {
+    const response = await api.get('/api/blogs')
+  
+    const titles = response.body.map(r => r.title)
+    expect(titles).toContain('potato')
+  })
 })
 
-test('all blogs are returned', async () => {
-  const response = await api.get('/api/blogs')
-
-  expect(response.body).toHaveLength(helper.initialBlogs.length)
-})
-
-test('a specific title is within the returned blogs', async () => {
-  const response = await api.get('/api/blogs')
-
-  const titles = response.body.map(r => r.title)
-  expect(titles).toContain('potato')
-})
-
-test('a valid blog can be added', async () => {
-  const newBlog = 
-    {
-      'title':'A new note',
-      'author':'Mark Zuckerberg',
-      'url':'https://www.facebook.com.br',
-      'likes':10
+describe('addition of new blogs' , () => {
+  test('a valid blog can be added', async () => {
+    const newBlog = 
+      {
+        'title':'A new note',
+        'author':'Mark Zuckerberg',
+        'url':'https://www.facebook.com.br',
+        'likes':10
+      }
+    
+    await api
+      .post('/api/blogs/')
+      .send(newBlog)
+      .expect(201)
+      .expect('Content-Type', /application\/json/)
+  
+    const blogsAtEnd = await helper.blogsInDb()
+  
+    expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1)
+  
+    const titles = blogsAtEnd.map(blog => blog.title)
+    expect(titles).toContain('A new note')
+  })
+  
+  test('a valid blog can be added with no likes by default', async () => {
+    const newBlog = 
+      {
+        'title':'A new note',
+        'author':'Mark Zuckerberg',
+        'url':'https://www.facebook.com.br'
+      }
+    
+    await api
+      .post('/api/blogs/')
+      .send(newBlog)
+      .expect(201)
+      .expect('Content-Type', /application\/json/)
+  
+    const blogsAtEnd = await helper.blogsInDb()
+  
+    expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1)
+  
+    const likes = blogsAtEnd.map(blog => blog.likes)
+    expect(likes).toContain(0)
+  })
+  
+  test('blog without content is not added', async () => {
+    const newBlog = {
+      'author':'Mark Zuckerberg'
     }
   
-  await api
-    .post('/api/blogs/')
-    .send(newBlog)
-    .expect(201)
-    .expect('Content-Type', /application\/json/)
-
-  const blogsAtEnd = await helper.blogsInDb()
-
-  expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1)
-
-  const titles = blogsAtEnd.map(blog => blog.title)
-  expect(titles).toContain('A new note')
-})
-
-test('a valid blog can be added with no likes by default', async () => {
-  const newBlog = 
-    {
-      'title':'A new note',
-      'author':'Mark Zuckerberg',
-      'url':'https://www.facebook.com.br'
-    }
+    await api
+      .post('/api/blogs/')
+      .send(newBlog)
+      .expect(400)
   
-  await api
-    .post('/api/blogs/')
-    .send(newBlog)
-    .expect(201)
-    .expect('Content-Type', /application\/json/)
-
-  const blogsAtEnd = await helper.blogsInDb()
-
-  expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1)
-
-  const likes = blogsAtEnd.map(blog => blog.likes)
-  expect(likes).toContain(0)
-})
-
-test('blog without content is not added', async () => {
-  const newBlog = {}
-
-  await api
-    .post('api/blogs/')
-    .send(newBlog)
-    .expect(400)
-
-  const blogsAtEnd = await helper.blogsInDb()
-  expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length)
+    const blogsAtEnd = await helper.blogsInDb()
+    expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length)
+  })
 })
 
 afterAll(() => {
